8b35281e5993f1bc9f1d371142a7fce5ace27dff
0lhakitura <okitura96@gmail.com>
2025-04-18 19:55:14 +0300

    Added template files

--- lec02/hw/job1/dal/sales_api.py
+++ lec02/hw/job1/dal/sales_api.py
@@ -1,30 +1,34 @@
+import requests
 from typing import List, Dict, Any
 
 API_URL = 'https://fake-api-vycpfa6oca-uc.a.run.app/'
 
 
-def get_sales(date: str) -> List[Dict[str, Any]]:
+def get_sales(date: str) -> List[List[Dict[str, Any]]]:
     """
-    Get data from sales API for specified date.
+    Fetch paginated sales data from the API for the given date.
 
-    :param date: data retrieve the data from
-    :return: list of records
+    :param date: The date for which sales data should be retrieved.
+    :return: List of pages with sales records.
     """
-    # TODO: implement me
+    all_pages = []
+    page = 1
 
-    # dummy return:
-    return [
-        {
-            "client": "Tara King",
-            "purchase_date": "2022-08-09",
-            "product": "Phone",
-            "price": 1062
-        },
-        {
-            "client": "Lauren Hawkins",
-            "purchase_date": "2022-08-09",
-            "product": "TV",
-            "price": 1373
-        },
-        # ...
-    ]
+    while True:
+        try:
+            # Fetch one page of data
+            response = requests.get(f"{API_URL}sales", params={"date": date, "page": page})
+            response.raise_for_status()
+            page_data = response.json()
+
+            if not page_data:
+                break  # Stop if no data in this page
+
+            all_pages.append(page_data)
+            page += 1
+
+        except requests.RequestException as e:
+            print(f"Error fetching page {page} of sales data: {e}")
+            break
+
+    return all_pages
